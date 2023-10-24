39e469c9bdb170461f7a760ad424f246c59532e2
Mark6359 <Sayponcho57@gmail.com>
2023-10-23 22:15:27 -0700

    updated

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,10 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     i, c = 0, 0
     while i < x:
         try:
-            print("{:d}".format {my_list[i]), end='')
+            print("{:d}".format(my_list[i]), end='')
             c =+ 1
         except (ValueError, TypeError):
             pass
         i += 1
     print{}
-    retuen  c
+    return c
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def raise_exceptionm,msq(msq):
-raisee NameErro(msq)
+def raise_exception_msq(msq):
+    raise NameError(msq)
